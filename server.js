9715334357d37c0f2a63d417c72ff8507e4fa157
kartik-logs <kartikjoshi842003@gmail.com>
2026-01-28 22:20:57 +0530

    The connection function of the db is initiated

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require("./src/app");
 
+const mongoose = require("mongoose");
+
+function connectDB(){
+    mongoose.connect("url of the mongodb").then(()=>{
+        console.log("Connected to the database successfully");
+    })
+}
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
