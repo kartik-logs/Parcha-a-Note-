c826e84f2598a09d007303ea2f24bfd5d400c8d5
kartik-logs <kartikjoshi842003@gmail.com>
2026-02-05 09:52:14 +0530

    GET api is ready

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 const app =  require("app");
+const connectToDb = require("./src/config/database")
 
+connectToDb()
 app.listen(process.env.PORT, ()=>{
     console.log("server is connected")
 })
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,18 @@
+import noteModel from "./models/notes.models";
+
 const express = require("express");
 const app = express();
+
+
+//GET api
+
+app.get("api/notes", async (req,res)=>{
+    const notes = await noteModel.find();
+
+    res.status(200).json({
+        Message: "notes fetched successfully",
+        data : notes
+    });
+});
 
 module.exports = app
